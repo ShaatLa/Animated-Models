13088b9f6f00504988bf1c6d941a56f7a7488f19
Mochalov, Dmitry <dmitry.mochalov@auriga.com>
2018-10-10 13:11:39 +0300

    Initial commit

---
+++ app/src/main/java/com/company/animatedmodels/MainActivity.java
@@ -0,0 +1,19 @@
+package com.company.animatedmodels;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
+    }
+}
