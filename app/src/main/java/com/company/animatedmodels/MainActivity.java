f9a25578f667258215327ec44fb0860a72964ef7
Mochalov, Dmitry <dmitry.mochalov@auriga.com>
2018-10-15 15:48:09 +0300

    Added self made island model with independent animated clouds, previous island model is deleted.

--- app/src/main/java/com/company/animatedmodels/MainActivity.java
+++ app/src/main/java/com/company/animatedmodels/MainActivity.java
@@ -20,6 +20,7 @@ import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.QuaternionEvaluator;
 import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.math.Vector3Evaluator;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
@@ -28,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private final String TAG = "Debug";
 
     ArFragment arFragment;
-    ModelRenderable modelRenderable;
+    ModelRenderable islandRenderable;
+    ModelRenderable cloudsRenderable;
 
 
     @Override
@@ -36,14 +38,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView cancel = (ImageView) findViewById(R.id.cancelButton);
+        ImageView cancel = findViewById(R.id.cancelButton);
         cancel.setVisibility(ImageView.GONE);
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
 
         ModelRenderable.builder()
-                .setSource(this, Uri.parse("island.sfb"))
+                .setSource(this, Uri.parse("mountains.sfb"))
                 .build()
-                .thenAccept(renderable -> modelRenderable = renderable)
+                .thenAccept(renderable -> islandRenderable = renderable)
+                .exceptionally(throwable -> {
+                    Log.d(TAG, "Couldn't create renderable...");
+                    throwable.printStackTrace();
+                    return null;
+                });
+
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("clouds.sfb"))
+                .build()
+                .thenAccept(renderable -> cloudsRenderable = renderable)
                 .exceptionally(throwable -> {
                     Log.d(TAG, "Couldn't create renderable...");
                     return null;
@@ -51,7 +63,8 @@ public class MainActivity extends AppCompatActivity {
 
         arFragment.setOnTapArPlaneListener(
                 (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-                    if (modelRenderable == null) {
+                    if (islandRenderable == null || cloudsRenderable == null) {
+                        Log.d(TAG, "Models is not ready");
                         return;
                     }
 
@@ -60,21 +73,22 @@ public class MainActivity extends AppCompatActivity {
                     arFragment.getArSceneView().getScene().addChild(anchorNode);
 
                     TransformableNode islandNode = new TransformableNode(arFragment.getTransformationSystem());
-                    islandNode.setRenderable(modelRenderable);
+                    TransformableNode cloudsNode = new TransformableNode(arFragment.getTransformationSystem());
+                    islandNode.setRenderable(islandRenderable);
+                    cloudsNode.setRenderable(cloudsRenderable);
+                    cloudsNode.setLocalPosition(new Vector3(0, 0.7f, 0));
+                    islandNode.addChild(cloudsNode);
                     anchorNode.addChild(islandNode);
 
                     showCancelButton(cancel, islandNode, anchorNode);
 
-                    islandNode.setOnTapListener(new Node.OnTapListener() {
-                        @Override
-                        public void onTap(HitTestResult hitTestResult, MotionEvent motionEvent) {
-                            showCancelButton(cancel, islandNode, anchorNode);
-                        }
-                    });
+                    islandNode.setOnTapListener((hitTestResult, motionEvent1) -> showCancelButton(cancel, islandNode, anchorNode));
 
-                    ObjectAnimator animator = createAnimator();
-                    animator.setTarget(islandNode);
-                    animator.start();
+                    ObjectAnimator levitateAnimator = createLevitateAnimator(islandNode);
+                    ObjectAnimator roundAnimator = createRoundAnimator(cloudsNode);
+                    roundAnimator.start();
+                    levitateAnimator.start();
+                    arFragment.getPlaneDiscoveryController().hide();
                 }
         );
     }
@@ -82,34 +96,44 @@ public class MainActivity extends AppCompatActivity {
     public void showCancelButton(ImageView button, TransformableNode nodeToDelete, AnchorNode parentNode)
     {
         button.setVisibility(ImageView.VISIBLE);
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                arFragment.getArSceneView().getScene().getChildren().contains(nodeToDelete);
-                arFragment.getArSceneView().getScene().removeChild(parentNode);
-                button.setVisibility(ImageView.GONE);
-            }
+        button.setOnClickListener(v -> {
+            arFragment.getArSceneView().getScene().getChildren().contains(nodeToDelete);
+            arFragment.getArSceneView().getScene().removeChild(parentNode);
+            button.setVisibility(ImageView.GONE);
         });
     }
 
-    private static ObjectAnimator createAnimator() {
-        Quaternion orientation1 = Quaternion.axisAngle(new Vector3(0.0f, 1.0f, 0.0f), 0);
-        Quaternion orientation2 = Quaternion.axisAngle(new Vector3(0.0f, 1.0f, 0.0f), 120);
-        Quaternion orientation3 = Quaternion.axisAngle(new Vector3(0.0f, 1.0f, 0.0f), 240);
-        Quaternion orientation4 = Quaternion.axisAngle(new Vector3(0.0f, 1.0f, 0.0f), 360);
+    private static ObjectAnimator createLevitateAnimator(TransformableNode targetNode) {
+        ObjectAnimator levitateAnimation = new ObjectAnimator();
+        levitateAnimation.setObjectValues(new Vector3(0f, 0f, 0f), new Vector3(0f, 0.015f, 0f));
+        levitateAnimation.setEvaluator(new Vector3Evaluator());
+        levitateAnimation.setPropertyName("localPosition");
+        levitateAnimation.setRepeatCount(ObjectAnimator.INFINITE);
+        levitateAnimation.setRepeatMode(ObjectAnimator.REVERSE);
+        levitateAnimation.setInterpolator(new LinearInterpolator());
+        levitateAnimation.setAutoCancel(true);
+        levitateAnimation.setDuration(3000);
+        levitateAnimation.setTarget(targetNode);
+        return levitateAnimation;
+    }
 
+    private static ObjectAnimator createRoundAnimator(TransformableNode targetNode) {
         ObjectAnimator roundAnimation = new ObjectAnimator();
-        roundAnimation.setObjectValues(orientation1, orientation2, orientation3, orientation4);
 
-        roundAnimation.setPropertyName("localRotation");
+        Vector3 horizontalVector = new Vector3(0, 1, 0);
+        Quaternion quaternion1 = Quaternion.axisAngle(horizontalVector, 0);
+        Quaternion quaternion2 = Quaternion.axisAngle(horizontalVector, 120);
+        Quaternion quaternion3 = Quaternion.axisAngle(horizontalVector, 240);
+        Quaternion quaternion4 = Quaternion.axisAngle(horizontalVector, 360);
 
+        roundAnimation.setObjectValues(quaternion1, quaternion2, quaternion3, quaternion4);
         roundAnimation.setEvaluator(new QuaternionEvaluator());
-
+        roundAnimation.setPropertyName("localRotation");
         roundAnimation.setRepeatCount(ObjectAnimator.INFINITE);
-        roundAnimation.setRepeatMode(ObjectAnimator.RESTART);
         roundAnimation.setInterpolator(new LinearInterpolator());
         roundAnimation.setAutoCancel(true);
         roundAnimation.setDuration(30000);
+        roundAnimation.setTarget(targetNode);
 
         return roundAnimation;
     }
